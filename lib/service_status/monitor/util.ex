6f2560ad190b9cffc7e27562628becb49166e99f
David Robertsson <david.robertsson@gmail.com>
2024-06-04 21:32:46 +0200

    formatting.

--- lib/service_status/monitor/util.ex
+++ lib/service_status/monitor/util.ex
@@ -1,11 +1,11 @@
 defmodule ServiceStatus.Monitor.Util do
-
-  def is_ok(url) do 
+  def is_ok(url) do
     status = Req.get!(url) |> Map.get(:status)
 
-    case status do 
-      200 -> 
+    case status do
+      200 ->
         true
+
       _ ->
         false
     end
