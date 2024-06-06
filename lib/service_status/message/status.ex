d43fe2f3ea784024d1de228b371c660615d0e2b3
David Robertsson <david.robertsson@gmail.com>
2024-06-06 13:59:27 +0200

    added field to status response

--- lib/service_status/message/status.ex
+++ lib/service_status/message/status.ex
@@ -3,15 +3,17 @@ defmodule ServiceStatus.Message.Status do
   @type url :: String.t()
   @type time :: integer()
   @type unit :: atom()
+  @type id :: integer() | nil
 
   @type t :: %__MODULE__{
           alias: alias(),
           url: url(),
           response_time: time(),
           time_unit: unit(),
-          ok: boolean()
+          ok: boolean(),
+          id: id()
         }
 
   @enforce_keys [:alias, :url, :response_time, :time_unit, :ok]
-  defstruct [:alias, :url, :response_time, :time_unit, :ok]
+  defstruct [:alias, :url, :response_time, :time_unit, :ok, id: nil]
 end
