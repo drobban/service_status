ff0badc934b85fc2150ad2981b23610458b20344
David Robertsson <david.robertsson@gmail.com>
2024-06-07 21:27:57 +0200

    One request
    Give response time and stat in one request

--- lib/service_status/monitor/util.ex
+++ lib/service_status/monitor/util.ex
@@ -26,6 +26,36 @@ defmodule ServiceStatus.Monitor.Util do
     end
   end
 
+  def response_stat(url, unit \\ :millisecond) do
+    req_date = DateTime.now!("Etc/UTC")
+    {status, resp} =
+      Req.get(url, max_retries: 0, pool_timeout: 1000, connect_options: [timeout: 500])
+    response_date = DateTime.now!("Etc/UTC")
+
+    response_status =
+      if status == :ok do
+        resp |> Map.get(:status)
+      else
+        0
+      end
+
+    td = DateTime.diff(response_date, req_date, unit)
+    case response_status do
+      200 ->
+        {td, true}
+
+      404 = not_found ->
+        Logger.debug("Page not found #{not_found} from - #{url}")
+        {td, false}
+
+      unknown ->
+        Logger.debug("Got: #{unknown} from - #{url}")
+        {td, false}
+    end
+  end
+
+
+
   def response_time(url, unit \\ :millisecond) do
     req_date = DateTime.now!("Etc/UTC")
     Req.get(url, max_retries: 0, pool_timeout: 1000, connect_options: [timeout: 500])
